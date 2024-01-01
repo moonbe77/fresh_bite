21d24e195383ab24d505d850a9cc1f99e5133778
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2024-01-02 09:33:44 +1100

    Update dependencies and add new components
    add dark theme toggle
    add icons
    add logo
    add menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+      },
+    }),
     tailwind({
       applyBaseStyles: false,
     }),
